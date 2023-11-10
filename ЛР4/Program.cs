35e06da706a8b26973a6c8ad0c7e49829069d620
AleksandrDubravskij <150442501+AleksandrDubravskij@users.noreply.github.com>
2023-11-10 15:28:16 +0300

    Initial commit

---
+++ ЛР4/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ЛР4
+{
+    using System;
+
+    class Program
+    {
+        static int SwapFirstAndLastDigit(int num)
+        {
+            int last = num % 10;
+            int middle = (num / 10) % 10;
+            int first = num / 100;
+
+            return last * 100 + middle * 10 + first;
+        }
+
+        static void Main()
+        {
+            Console.Write("Введите трехзначное число: ");
+            int number = int.Parse(Console.ReadLine());
+
+            int swappedNumber = SwapFirstAndLastDigit(number);
+            Console.WriteLine($"Число после замены: {swappedNumber}");
+        }
+    }
+
+}
